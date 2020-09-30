141ff3ac7fbb377b45d9d6e4a5ee2d91cbd6c322
syahjamal <sulaimansyahjamal@gmail.com>
2020-09-30 23:21:14 +0700

    Notification.js update

--- frontend/src/components/Notifications.js
+++ frontend/src/components/Notifications.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import { sendMsg } from '../api';
 
 class Notifications extends Component {
     constructor(props) {
         super(props);
-        let temp = sendMsg(JSON.parse(this.props.notifications));
+        let temp = JSON.parse(this.props.notifications);
         this.state = {
             notifications: temp
         }
@@ -13,7 +12,7 @@ class Notifications extends Component {
     render() {
         return (
             <div className='Notifications'>
-                {this.state.notifications.message}
+                {this.state.notifications}
             </div>
         );
     };
