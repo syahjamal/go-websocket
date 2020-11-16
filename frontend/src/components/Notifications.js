8669bf03acd0fe034d4015499c424df1ac710509
syahjamal <sulaimansyahjamal@gmail.com>
2020-11-16 13:54:28 +0700

    Notification.js change

--- frontend/src/components/Notifications.js
+++ frontend/src/components/Notifications.js
@@ -3,11 +3,7 @@ import React, { Component } from 'react';
 class Notifications extends Component {
     constructor(props) {
         super(props);
-<<<<<<< HEAD
-        let temp = JSON.parse(this.props.notifications);
-=======
         let temp = JSON.parse(this.props.notification);
->>>>>>> ca5d2deed5da57bd2cf5e270e0158dc360ee032c
         this.state = {
             notification: temp
         }
@@ -16,11 +12,7 @@ class Notifications extends Component {
     render() {
         return (
             <div className='Notifications'>
-<<<<<<< HEAD
-                {this.state.notifications}
-=======
                 {this.state.notification.message}
->>>>>>> ca5d2deed5da57bd2cf5e270e0158dc360ee032c
             </div>
         );
     };
